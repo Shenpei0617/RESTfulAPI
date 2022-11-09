61505ef073f7b95bb8c6246e0845e710af726bb8
Shen pei <shen.pei.wun@gmail.com>
2022-11-09 12:14:39 +0800

    1109-登入

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+
+
+const AuthContext = createContext({});
+
+export default AuthContext;
+
+export const AuthContextProvider = function ({children}){
+    //const [theme, setTheme] = useState(themes.light);
+    let initAuth
+
+    // TODO: 取得目前狀態
+    // 2. 登入: 成功, 失敗
+    // TODO: 登出
+
+
+    return (
+        <AuthContext.Provider value={{}}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
