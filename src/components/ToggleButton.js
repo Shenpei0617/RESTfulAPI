292a1de19d602a994f43c9460018e69175a609f9
Shen pei <shen.pei.wun@gmail.com>
2022-11-09 12:12:54 +0800

    theme功能在Navbar

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,20 +1,27 @@
 import { useState } from "react";
 
- function ToggleButton({statusIndex ,texts}) {
+
+ function ToggleButton({statusIndex ,texts, handler }) {
     console.log({statusIndex})
     const [myIndex, setMyIndex] = useState(statusIndex);
+   
     const classNames = ["btn"];
     //const text = myIndex === 0 ? "暗" : "亮";
 
     classNames.push(!myIndex ? 'btn-primary' : 'btn-outline-primary');
+    const myClick=()=>{
+        const nextStatusIndex = myIndex === 0 ? 1 : 0;
+        setMyIndex(nextStatusIndex);
+        handler(nextStatusIndex);
+    }
 
   return (
     <>
         <button 
         type="button" 
         className={classNames.join(' ')}
-        onClick={()=>setMyIndex(myIndex ===0?1:0)}
-        > {texts [myIndex]}
+        onClick={myClick}
+        > {texts[myIndex]}
         </button>
     </>
   )
@@ -23,6 +30,7 @@ import { useState } from "react";
 ToggleButton.defaultProps = {
     statusIndex: 0,
     texts: ["關", "開"],
+    handler: (index) => {},
 }
 
 export default ToggleButton
