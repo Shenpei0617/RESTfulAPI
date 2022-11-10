c82e50120a7b92cda49fae58efc2cbc6763937d4
Shen pei <shen.pei.wun@gmail.com>
2022-11-10 10:53:07 +0800

    1110-canvas引入圖片

--- src/pages/Canavas.js
+++ src/pages/Canavas.js
@@ -1,10 +1,36 @@
+import { useEffect, useRef ,useState} from "react";
+import{productData}from "./../data/product-data"
+
 function Canvas1() {
+
+    const cRef = useRef()
+    
+
+    useEffect(()=>{
+        const ctx = cRef.current.getContext('2d');
+       
+
+        const img = new Image();
+        img.onload=()=>{
+            ctx.drawImage(img, 0, 0);
+        }
+        img.src = './images/dish.jpeg';
+    },[])
+
     return (
       <div className="container">
+      <div className="row">
+        <div className="col">
+            {productData.map((el)=>{
+                return <img src={"images/"+el.img} alt={el.name} key={el.id} width="150"/>
+            })}
+        </div>
+      </div>
         <canvas
-          width="800"
-          height="600"
-          style={{ border: "1px solid blue" }}
+        ref={cRef}
+          width="700"
+          height="700"
+        //   style={{ border: "1px solid blue" }}
         ></canvas>
       </div>
     );
